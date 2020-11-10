953ab108affd6ff823d181e0ce846193eb4d12d0
Zadoev123 <i.zadoev@yandex.ru>
2020-11-10 19:11:23 +0300

    Дописаны 2 фразы

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Hellow World");
 	System.out.println("Example");
+        System.out.println("Еще раз!!");
     }
 }
